b8308bb12951163cf60b5bccdbcb742e8f23de18
MaxPacheco02 <a01552369@tec.mx>
2023-01-30 20:07:04 -0600

    Working roslaunch, need documentation

--- xbee_tcp_client.py
+++ xbee_tcp_client.py
@@ -4,36 +4,30 @@ import sys
 import os
 from digi.xbee.devices import XBeeDevice, TimeoutException
 
+#Print usage
 if len(sys.argv) < 2:
     "Not enough arguments\n\nUsage:\n\npython3 roboshell_host.py XBEE_PORT\n"
 xbee_port = sys.argv[1]
 remote_id = "STATION_XBEE"
 
+#Get os-dependant path
 wd = os.getcwd()
 remote_device = 0
 
 device = XBeeDevice(xbee_port, 9600)
+device.set_sync_ops_timeout(5)
 print("Bound to boat xbee")
 
+#Main function for data_processing
 def data_callback(xbee_message):
     global wd
     global remote_device
-    data = xbee_message.data.decode()
+    data = xbee_message.data.decode().replace("°","<")
     print("Received command '", data, "'")
     try:
         data_list = list(map(str.strip, data.split()))
         print(data_list)
-        '''
-        if len(data_list) > 1 and data_list[0] == 'cd':
-            if data_list[1] == '..':
-                wd = wd.split('/')
-                wd.pop()
-                wd = '/'.join(wd)
-                print(wd)
-            else:
-                data_list.pop(0)
-                wd = data_list[0]
-        '''
+        
         if data_list[0] == 'cd':
             try:
                 data_list.pop(0)
@@ -41,6 +35,15 @@ def data_callback(xbee_message):
                 device.send_data(remote_device, '\nChdir ok')    
             except IndexError:
                 device.send_data(remote_device, wd)    
+        elif data_list[0] == 'ash':
+            try:
+                data_list.pop(0)
+                data_list = ['/bin/sh'] + [(" < " + data_list[0])]
+                print(data_list)
+                subprocess.call(data_list, shell=True)
+                device.send_data(remote_device, '\nSent process to sh')    
+            except IndexError:
+                device.send_data(remote_device, "Index e")    
         elif data_list[0] == 'ser':
             print("ROS command")
             data_list.pop(0)
@@ -53,13 +56,13 @@ def data_callback(xbee_message):
                 device.send_data(remote_device, line.strip())
         else:
             print("Normal command")
-            proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, cwd=wd)
+            proc = subprocess.Popen(data, shell=True,stdout=subprocess.PIPE, cwd=wd)
             tmp = proc.stdout.read()
             device.send_data(remote_device, tmp)
 
     except TimeoutException:
         print("Process startup failed")
-        device.send_data(remote_device, "Command failed")
+        #device.send_data(remote_device, "Command failed")
     except Exception as e:
         print("Exception raised:", e)
         device.send_data(remote_device, str(e))
